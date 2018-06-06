d2a8463548bc02e93e785cb21452e4c6b092f52c
meredithwinchell <meredithwinchell@gmail.com>
2018-06-06 20:21:00 +0000

    Done.

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,18 +1,63 @@
 
 def create_olympics_hash
-  # Implement this method so that it returns a hash with the data provided on README.md
+  summer_olympics = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  return summer_olympics
 end
+
 
 def add_a_key_value_pair
-  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+  summer_olympics = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  summer_olympics[:Atlanta] = "1996"
+  return summer_olympics
+
 end
+
+  # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
+
 
 def iterate_through_hash
+  summer_olympics = {
+    :Sydney => "2000",
+    :Athens => "2004",
+    :Beijing => "2008",
+    :London => "2012"
+  }
+  summer_olympics[:Atlanta] = "1996"
+
+  summer_olympics.each do |place, year|
+    puts "The #{place} summer olympics took place in #{year}."
+  end
   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+puts summer_olympics  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
 end
 
-def iterate_through_keys
+
+
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
-  # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
+  def iterate_through_keys
+    summer_olympics = {
+      :Sydney => "2000",
+      :Athens => "2004",
+      :Beijing => "2008",
+      :London => "2012"
+    }
+    summer_olympics[:Atlanta] = "1996"
+
+    summer_olympics.each_key do |place|
+      upcased_cities = "#{place}".upcase
+      puts upcased_cities
+    end
+
 end
+
+iterate_through_keys
